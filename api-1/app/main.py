cb080df52fdfda1150a3ced56e85ce92a7602477
Harsh Raj <harshraj4308g@gmail.com>
2026-08-01 14:17:35 +0530

    Fix CORS: explicitly allow localhost origins with credentials

--- api-1/app/main.py
+++ api-1/app/main.py
@@ -44,10 +44,16 @@ app = FastAPI(
 )
 
 # Enable CORS for Frontend Development
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "*",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
